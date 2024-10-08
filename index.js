3d90394b40f6a602fc82627d277688eb1f58dd5d
Nell David Soledad <nelldnine@gmail.com>
2024-10-08 10:35:11 +0800

    Initial commit

---
+++ index.js
@@ -0,0 +1,25 @@
+require('dotenv').config();
+
+const express = require('express');
+const cors = require('cors');
+const { createProxyMiddleware } = require('http-proxy-middleware');
+
+const simpleRequestLogger = (proxyServer, options) => {
+  proxyServer.on('proxyReq', (proxyReq, req, res) => {
+    console.log(`${new Date().toISOString()} ${req.method} ${req.url}`);
+  });
+};
+
+const app = express();
+
+app.use(cors());
+app.use(
+  '/',
+  createProxyMiddleware({
+    target: process.env.URL,
+    changeOrigin: true,
+    plugins: [simpleRequestLogger],
+  })
+);
+
+app.listen(process.env.URL || 3000);
